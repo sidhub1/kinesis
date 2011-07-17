a77c0b0959a2a9b62066d3ba51801d1ca70db35b
sandu.albu <sandu.albu@gmail.com>
2011-07-17 22:50:38 +0000

    project base

---
+++ KineSis/KineSis/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿/*
+   Copyright 2011 Alexandru Albu - http://code.google.com/p/kinesis/
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.Kinect.Nui;
+using KineSis.Utils;
+
+namespace KineSis {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        Runtime nui;
+        int totalFrames = 0;
+        int lastFrames = 0;
+        Boolean fullscreen = false;
+        Double originalWidth;
+        Double originalHeight;
+        Double originalLeft;
+        Double originalTop;
+        DateTime lastTime = DateTime.MaxValue;
+        SettingsWindow settingsWindow = new SettingsWindow();
+
+        // We want to control how depth data gets converted into false-color data
+        // for more intuitive visualization, so we keep 32-bit color frame buffer versions of
+        // these, to be updated whenever we receive and process a 16-bit frame.
+        const int RED_IDX = 2;
+        const int GREEN_IDX = 1;
+        const int BLUE_IDX = 0;
+        byte[] depthFrame32 = new byte[320 * 240 * 4];
+
+
+        Dictionary<JointID, Brush> jointColors = new Dictionary<JointID, Brush>() { 
+            {JointID.HipCenter, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
+            {JointID.Spine, new SolidColorBrush(Color.FromRgb(169, 176, 155))},
+            {JointID.ShoulderCenter, new SolidColorBrush(Color.FromRgb(168, 230, 29))},
+            {JointID.Head, new SolidColorBrush(Color.FromRgb(200, 0,   0))},
+            {JointID.ShoulderLeft, new SolidColorBrush(Color.FromRgb(79,  84,  33))},
+            {JointID.ElbowLeft, new SolidColorBrush(Color.FromRgb(84,  33,  42))},
+            {JointID.WristLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
+            {JointID.HandLeft, new SolidColorBrush(Color.FromRgb(215,  86, 0))},
+            {JointID.ShoulderRight, new SolidColorBrush(Color.FromRgb(33,  79,  84))},
+            {JointID.ElbowRight, new SolidColorBrush(Color.FromRgb(33,  33,  84))},
+            {JointID.WristRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
+            {JointID.HandRight, new SolidColorBrush(Color.FromRgb(37,   69, 243))},
+            {JointID.HipLeft, new SolidColorBrush(Color.FromRgb(77,  109, 243))},
+            {JointID.KneeLeft, new SolidColorBrush(Color.FromRgb(69,  33,  84))},
+            {JointID.AnkleLeft, new SolidColorBrush(Color.FromRgb(229, 170, 122))},
+            {JointID.FootLeft, new SolidColorBrush(Color.FromRgb(255, 126, 0))},
+            {JointID.HipRight, new SolidColorBrush(Color.FromRgb(181, 165, 213))},
+            {JointID.KneeRight, new SolidColorBrush(Color.FromRgb(71, 222,  76))},
+            {JointID.AnkleRight, new SolidColorBrush(Color.FromRgb(245, 228, 156))},
+            {JointID.FootRight, new SolidColorBrush(Color.FromRgb(77,  109, 243))}
+        };
+
+        private void Window_Loaded(object sender, EventArgs e) {
+            nui = new Runtime();
+
+            try {
+                nui.Initialize(RuntimeOptions.UseSkeletalTracking);
+            } catch (InvalidOperationException) {
+                System.Windows.MessageBox.Show("Runtime initialization failed. Please make sure Kinect device is plugged in.");
+                return;
+            }
+
+            nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+        }
+
+
+        private Point getDisplayPosition(Joint joint) {
+            float depthX, depthY;
+            nui.SkeletonEngine.SkeletonToDepthImage(joint.Position, out depthX, out depthY);
+            depthX = Math.Max(0, Math.Min(depthX * 320, 320));  //convert to 320, 240 space
+            depthY = Math.Max(0, Math.Min(depthY * 240, 240));  //convert to 320, 240 space
+            int colorX, colorY;
+            ImageViewArea iv = new ImageViewArea();
+            // only ImageResolution.Resolution640x480 is supported at this point
+            nui.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(ImageResolution.Resolution640x480, iv, (int)depthX, (int)depthY, (short)0, out colorX, out colorY);
+
+            // map back to skeleton.Width & skeleton.Height
+            return new Point((int)( userCanvas.Width * colorX / 640.0 ), (int)( userCanvas.Height * colorY / 480 ));
+        }
+
+        Polyline getBodySegment(Microsoft.Research.Kinect.Nui.JointsCollection joints, Brush brush, params JointID[] ids) {
+            PointCollection points = new PointCollection(ids.Length);
+            for (int i = 0; i < ids.Length; ++i) {
+                points.Add(getDisplayPosition(joints[ids[i]]));
+            }
+
+            Polyline polyline = new Polyline();
+            polyline.Points = points;
+            polyline.Stroke = brush;
+            polyline.StrokeThickness = 5;
+            return polyline;
+        }
+
+        void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e) {
+            SkeletonFrame skeletonFrame = e.SkeletonFrame;
+            int iSkeleton = 0;
+            Brush[] brushes = new Brush[6];
+            brushes[0] = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+            brushes[1] = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+            brushes[2] = new SolidColorBrush(Color.FromRgb(64, 255, 255));
+            brushes[3] = new SolidColorBrush(Color.FromRgb(255, 255, 64));
+            brushes[4] = new SolidColorBrush(Color.FromRgb(255, 64, 255));
+            brushes[5] = new SolidColorBrush(Color.FromRgb(128, 128, 255));
+
+            userCanvas.Children.Clear();
+            foreach (SkeletonData data in skeletonFrame.Skeletons) {
+                if (SkeletonTrackingState.Tracked == data.TrackingState) {
+                    // Draw bones
+                    Brush brush = brushes[iSkeleton % brushes.Length];
+                    userCanvas.Children.Add(getBodySegment(data.Joints, brush, JointID.HipCenter, JointID.Spine, JointID.ShoulderCenter, JointID.Head));
+                    userCanvas.Children.Add(getBodySegment(data.Joints, brush, JointID.ShoulderCenter, JointID.ShoulderLeft, JointID.ElbowLeft, JointID.WristLeft, JointID.HandLeft));
+                    userCanvas.Children.Add(getBodySegment(data.Joints, brush, JointID.ShoulderCenter, JointID.ShoulderRight, JointID.ElbowRight, JointID.WristRight, JointID.HandRight));
+                    userCanvas.Children.Add(getBodySegment(data.Joints, brush, JointID.HipCenter, JointID.HipLeft, JointID.KneeLeft, JointID.AnkleLeft, JointID.FootLeft));
+                    userCanvas.Children.Add(getBodySegment(data.Joints, brush, JointID.HipCenter, JointID.HipRight, JointID.KneeRight, JointID.AnkleRight, JointID.FootRight));
+
+                    // Draw joints
+                    foreach (Joint joint in data.Joints) {
+                        Point jointPos = getDisplayPosition(joint);
+                        Line jointLine = new Line();
+                        jointLine.X1 = jointPos.X - 3;
+                        jointLine.X2 = jointLine.X1 + 6;
+                        jointLine.Y1 = jointLine.Y2 = jointPos.Y;
+                        jointLine.Stroke = jointColors[joint.ID];
+                        jointLine.StrokeThickness = 6;
+                        userCanvas.Children.Add(jointLine);
+                    }
+                }
+                iSkeleton++;
+            } // for each skeleton
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            nui.Uninitialize();
+            Environment.Exit(0);
+        }
+
+        private void UserCanvas_CM_FullScreen_Click(object sender, RoutedEventArgs e) {
+            if (!fullscreen) {
+                originalWidth = this.Width;
+                originalHeight = this.Height;
+                originalLeft = this.Left;
+                originalTop = this.Top;
+                userCanvas.Height = WindowUtils.Screens[WindowUtils.DetectScreen(this)].Bounds.Height;
+                userCanvas.Width = ( userCanvas.Height * 4 ) / 3;
+                Double marginLeft = ( WindowUtils.Screens[WindowUtils.DetectScreen(this)].Bounds.Width - userCanvas.Width ) / 2;
+                userCanvas.Margin = new Thickness(marginLeft, 0, 0, 0);
+                ( (System.Windows.Controls.MenuItem)userCanvas.ContextMenu.Items.GetItemAt(2) ).Header = "Exit Full Screen";
+            } else {
+                userCanvas.Height = 600;
+                userCanvas.Width = 800;
+                userCanvas.Margin = new Thickness(0, 0, 0, 0);
+                ( (System.Windows.Controls.MenuItem)userCanvas.ContextMenu.Items.GetItemAt(2) ).Header = "Full Screen";
+            }
+            WindowUtils.FullScreen(this, fullscreen, originalWidth, originalHeight, originalLeft, originalTop);
+            fullscreen = !fullscreen;
+        }
+
+        private void UserCanvas_CM_Open_Click(object sender, RoutedEventArgs e) {
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true) {
+                // Open document
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void UserCanvas_CM_Settings_Click(object sender, RoutedEventArgs e) {
+                settingsWindow = new SettingsWindow();
+                settingsWindow.Show();
+        }
+    }
+}
---
+++ KineSis/KineSis/Utils/WindowUtils.cs
@@ -0,0 +1,83 @@
+﻿/*
+   Copyright 2011 Alexandru Albu - http://code.google.com/p/kinesis/
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace KineSis.Utils {
+    static class WindowUtils {
+        public static Screen[] Screens {
+            get {
+                return Screen.AllScreens;
+            }
+        }
+
+        /// <summary>
+        /// Get the number of screen where the Window is shown
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        public static int DetectScreen(Window form) {
+            int screen = 0;
+
+            double right = form.Left + form.Width;
+            double left = form.Left;
+            double center = ( left + right ) / 2;
+            if (Screens[0].Bounds.Left <= center && center <= Screens[0].Bounds.Right) {
+                screen = 0;
+            } else {
+                screen = 1;
+            }
+            return screen;
+        }
+
+        /// <summary>
+        /// Switch a window to full screen state on the screen where currently is
+        /// </summary>
+        /// <param name="window">window to full screen</param>
+        /// <param name="fullscreen">boolean indicating the current state of the window</param>
+        /// <param name="originalWidth">original width of the window before full screen</param>
+        /// <param name="originalHeight">original height of the window before full screen</param>
+        /// <param name="originalLeft">original left of the window before full screen</param>
+        /// <param name="originalTop">original top of the window before full screen</param>
+        public static void FullScreen(Window window, Boolean fullscreen, Double originalWidth, Double originalHeight, Double originalLeft, Double originalTop) {
+            int screen = DetectScreen(window);
+            if (fullscreen == false) {
+                window.Topmost = true;
+                window.Left = Screens[screen].Bounds.Left;
+                window.Top = Screens[screen].Bounds.Top;
+                window.WindowStyle = WindowStyle.None;
+                window.Width = Screens[screen].Bounds.Width;
+                window.Height = Screens[screen].Bounds.Height;
+                window.ResizeMode = ResizeMode.NoResize;
+                fullscreen = true;
+            } else {
+                window.Topmost = false;
+                window.Left = originalLeft;
+                window.Top = originalTop;
+                window.WindowStyle = WindowStyle.SingleBorderWindow;
+                window.Width = originalWidth;
+                window.Height = originalHeight;
+                fullscreen = false;
+            }
+        }
+    }
+}
