87b7f79c667cac63e3adffab79a12b61303f33f5
sandu.albu@gmail.com <sandu.albu@gmail.com@2bc02b06-c632-0152-106a-faf27bdc20a7>
2011-12-08 19:08:44 +0000

    changed header

--- trunk/KineSis/KineSis/UserInterface/Entities/Groups/Generic.cs
+++ trunk/KineSis/KineSis/UserInterface/Entities/Groups/Generic.cs
@@ -1,5 +1,5 @@
 ﻿/*
-   Copyright 2011 Alexandru Albu - http://code.google.com/p/kinesis/
+   Copyright 2011 Alexandru Albu - sandu.albu@gmail.com
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
